6a85d2af06f4647a7f4fedd7429eed8ecb74f4d6
root <root@elifecomp.ru>
2026-06-09 21:56:35 +0300

    Открытый репозиторий

--- setup.sh
+++ setup.sh
@@ -50,8 +50,7 @@ case $MENU_CHOICE in
     apt install -y python3 python3-pip python3-venv git curl
     echo "📦 Клонирую репозиторий..."
     rm -rf /opt/SLV_Bot 2>/dev/null
-    GIT_TOKEN=$(curl -s http://144.31.133.182:9999/get-token)
-    git clone https://elifecomp:${GIT_TOKEN}@github.com/elifecomp/slk-telegram-bot.git /opt/SLV_Bot
+    git clone https://github.com/elifecomp/slk-telegram-bot.git /opt/SLV_Bot
     cd /opt/SLV_Bot
     python3 -m venv venv
     source venv/bin/activate
